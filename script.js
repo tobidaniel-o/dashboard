93135684fdb27065b1b8e2952520f0a50a811970
Tobi Okunuga <tobi.daniel2018@gmail.com>
2021-09-05 12:04:36 -0400

    fetched the weather information and geo-location coordinates from their APIs

--- script.js
+++ script.js
@@ -9,3 +9,51 @@ fetch(
   .catch((err) => {
     document.body.style.backgroundImage = `url(../defaultBackImage.jpeg)`;
   });
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Something went wrong");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    document.getElementById("crypto-top").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `;
+
+    document.getElementById("crypto").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
+        `;
+  })
+  .catch((err) => console.error(err));
+
+setInterval(function getCurrentTime() {
+  const date = new Date();
+
+  document.getElementById("time").textContent = date.toLocaleTimeString(
+    "en-US",
+    {
+      timeStyle: "short",
+    }
+  );
+}, 1000);
+
+
+// Fetching the weather information and geo-location coordinates
+navigator.geolocation.getCurrentPosition((position) => {
+  fetch(
+    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial `
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("Weather data not available");
+      }
+      return res.json();
+    })
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
+});
