5888442fb219f00ba022e0d5204bd99142d3a1e4
Tobi Okunuga <tobi.daniel2018@gmail.com>
2021-09-05 22:48:58 -0400

    project-complete. Added the dashbaord to the Chrome extension

--- script.js
+++ script.js
@@ -42,11 +42,10 @@ setInterval(function getCurrentTime() {
   );
 }, 1000);
 
-
 // Fetching the weather information and geo-location coordinates
 navigator.geolocation.getCurrentPosition((position) => {
   fetch(
-    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial `
+    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric `
   )
     .then((res) => {
       if (!res.ok) {
@@ -54,6 +53,13 @@ navigator.geolocation.getCurrentPosition((position) => {
       }
       return res.json();
     })
-    .then((data) => console.log(data))
+    .then((data) => {
+      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      document.getElementById("weather").innerHTML = `
+        <img src=${iconUrl} />
+        <p class="">${Math.round(data.main.temp)}º</p>
+        <p class="weather-city">${data.name}</p>
+      `;
+    })
     .catch((err) => console.error(err));
 });
